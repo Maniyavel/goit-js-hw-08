dfc4ea367dc1899ac951408a306d4ec0cc999660
Maniyavel <116820894+Maniyavel@users.noreply.github.com>
2023-04-11 15:55:41 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTime ="videoplayer-current-time";
+const localCurrentTime=localStorage.getItem(currentTime);
 
+    const onPlay = function(timeupdate) {
+        console.log(timeupdate);
+        localStorage.setItem(currentTime,timeupdate.seconds);
+    };
 
-export{}
+    player.on('timeupdate', throttle(onPlay, 1000));
+
+    player.setCurrentTime(30.456).then(function(seconds) {
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+    
+            default:
+                // some other error occurred
+                break;
+        }
+    });
